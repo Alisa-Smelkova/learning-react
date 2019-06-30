61016e356fe18034bedcefaa35352788308aa611
Alisa <alisa@Alisas-MacBook-Pro.local>
2019-06-30 22:48:31 +0300

    Created hero banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Ok, Start</h1>
-    <p>Example Change</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Ok, today I start learning React</h1>
+        <p>This is my first website built with React and Gatsby. This is the hero banner and a pretty stupid text in it.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
